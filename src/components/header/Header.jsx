c03c7a8d03f470832cf32612534626400213bbd0
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-02-18 22:22:01 +0600

    Added image and animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <Link to={"/index.html"} className="nav__logo">
+        <Link to={"/"} className="nav__logo">
           Saiful
         </Link>
 
@@ -59,9 +59,12 @@ const Header = () => {
             </li>
           </ul>
           {/* Close Icon */}
-          <i className="uil uil-times nav__close"></i>
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
         </div>
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i className="uil uil-apps"></i>
         </div>
       </nav>
