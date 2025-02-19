39c3c728d603326ef0d294dd145b3580b905c47b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-02-19 10:15:40 +0600

    Adding content on info component

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,11 +1,26 @@
-
-
 const Info = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="about__info grid">
+      {/* First about box */}
+      <div className="about__box">
+        <i className="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">8 Years Working</span>
+      </div>
+      {/* Second about box */}
+      <div className="about__box">
+        <i className="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">48 + Projects</span>
+      </div>
+      {/* Third about box */}
+      <div className="about__box">
+        <i className="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
