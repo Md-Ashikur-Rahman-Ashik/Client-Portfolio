3cc46f3d8e4ef14b04b5cd55288b14db70370b38
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-02-19 11:19:52 +0600

    Adding skills component

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,72 @@
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title"></h3>
+
+      <div className="skills__box">
+        {/* First skill group */}
+        <div className="skills__group">
+          {/* First skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Second skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Third skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+        </div>
+        {/* Second skill group */}
+        <div className="skills__group">
+          {/* First skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Second skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Third skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
---
+++ src/components/skills/Frontend.jsx
@@ -0,0 +1,72 @@
+const Frontend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Frontend developer</h3>
+
+      <div className="skills__box">
+        {/* First skill group */}
+        <div className="skills__group">
+          {/* First skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">HTML</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Second skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Third skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+        </div>
+        {/* Second skill group */}
+        <div className="skills__group">
+          {/* First skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Second skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+          {/* Third skill data */}
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frontend;
